f8f82184582e492daf33324c4d037badf8fa6ca5
Renan <orefusado@gmail.com>
2023-02-12 22:50:39 -0300

    mocha hooks

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,13 +1,34 @@
 const assert = require('assert');
 const Math = require('../src/Math.js');
 
+let value = 0;
+
 describe('Math class', function() {
+  // HOOKS
+
+  // runs before all tests in this file regardless where this line is defined.
+  // before(function() { });
+
+  // runs after all tests in this file
+  // after(function() { });
+
+  // runs before each test in this block
+  // beforeEach(function() { });
+
+  // runs after each test in this block
+  // afterEach(function() { });
+
+  beforeEach(function() {
+    value = 5;
+  });
+
+
   it('Sum two numbers', function(done) {
     const math = new Math();
     this.timeout(2500);
 
-    math.sum(5, 5, (value) => {
-      assert.equal(value, 10);
+    math.sum(value, value, (result) => {
+      assert.equal(result, 10);
       done();
     });
   });
@@ -15,16 +36,16 @@ describe('Math class', function() {
   it('Subtract two numbers'); // pending
 
   // test only this method
-  it.only('Multiply two numbers', function() {
-    const math = new Math();
+  // it.only('Multiply two numbers', function() {
+  //   const math = new Math();
 
-    assert.equal(math.multiply(5, 5), 25);
-  });
+  //   assert.equal(math.multiply(value, value), 25);
+  // });
 
   // skip and do not test this method
   it.skip('Divide two numbers', function() {
     const math = new Math();
 
-    assert.equal(math.divide(5, 5), 1);
+    assert.equal(math.divide(value, value), 1);
   });
 });
