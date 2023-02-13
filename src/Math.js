3125de07130e4c4be6f43f7c31f5fbd3cb2b03b6
Renan <orefusado@gmail.com>
2023-02-12 22:22:27 -0300

    mocha it methods

--- src/Math.js
+++ src/Math.js
@@ -2,7 +2,7 @@ class Math {
   sum(a, b, callback) {
     setTimeout(() => {
       callback(a + b);
-    }, 1000);
+    }, 2000);
   }
 }
 
--- test/math.spec.js
+++ test/math.spec.js
@@ -4,10 +4,27 @@ const Math = require('../src/Math.js');
 describe('Math class', function() {
   it('Sum two numbers', function(done) {
     const math = new Math();
+    this.timeout(2500);
 
     math.sum(5, 5, (value) => {
-      assert.equal(value, 15);
+      assert.equal(value, 10);
       done();
     });
+  });
+
+  it('Subtract two numbers'); // pending
+
+  // test only this method
+  it.only('Multiply two numbers', function() {
+    const math = new Math();
+
+    assert.equal(math.multiply(5, 5), 25);
+  });
+
+  // skip and do not test this method
+  it.skip('Divide two numbers', function() {
+    const math = new Math();
+
+    assert.equal(math.divide(5, 5), 1);
   });
 });
